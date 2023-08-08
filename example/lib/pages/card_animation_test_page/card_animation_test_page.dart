b6d7956437c324666132f82cf16e81187d1f8e91
orange-amuz <orange@amuz.co.kr>
2023-08-08 19:04:21 +0900

    카드 애니메이션 페이지 예제 수정

--- example/lib/pages/card_animation_test_page/card_animation_test_page.dart
+++ example/lib/pages/card_animation_test_page/card_animation_test_page.dart
@@ -1,6 +1,7 @@
 import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 class CardAnimationTestPage extends StatefulWidget {
   const CardAnimationTestPage({Key? key}) : super(key: key);
@@ -15,14 +16,17 @@ class _CardAnimationTestPage extends State<CardAnimationTestPage>
 
   static const cardAngle = math.pi / 12 * 5;
 
+  BoxConstraints constraints = const BoxConstraints();
+
   double xAngle = 0.0;
   double yAngle = 0.0;
-  double zAngle = 0.0;
+  // double zAngle = 0.0;
 
   double xAlign = 0;
   double yAlign = 0;
 
-  // double rotateAngle = 0;
+  double beforeX = 0;
+  double beforeY = 0;
 
   Curve curve = Curves.easeInOut;
 
@@ -36,6 +40,17 @@ class _CardAnimationTestPage extends State<CardAnimationTestPage>
     super.initState();
 
     animationController.reverseDuration = const Duration(milliseconds: 1000);
+
+    gyroscopeEvents.listen(
+      (event) {
+        // print(event.toString());
+
+        // updateValueForwardGyro(x: event.x, y: event.y);
+
+        beforeX = event.x;
+        beforeY = event.y;
+      },
+    );
 
     animationController.addListener(
       () {
@@ -51,18 +66,6 @@ class _CardAnimationTestPage extends State<CardAnimationTestPage>
     animationController.dispose();
   }
 
-  // void setTargetLeft() {
-  //   alignX = -1.0;
-  //   alignY = 2.0;
-  //   rotateAngle = math.pi / 6;
-  // }
-
-  // void setTargetRight() {
-  //   alignX = 1.0;
-  //   alignY = -2.0;
-  //   rotateAngle = -math.pi / 6;
-  // }
-
   void setCurveForward() {
     curve = Curves.easeInOut;
   }
@@ -71,11 +74,46 @@ class _CardAnimationTestPage extends State<CardAnimationTestPage>
     curve = Curves.bounceOut.flipped;
   }
 
-  void updateValueForward({
+  Future<void> updateValueForwardGyro({
+    required double x,
+    required double y,
+  }) async {
+    // final center = Offset(
+    //   constraints.maxWidth / 2,
+    //   constraints.maxHeight / 2,
+    // );
+
+    final relativeOffset = Offset(
+      x,
+      y,
+    );
+
+    final dx = relativeOffset.dx;
+    final dy = relativeOffset.dy;
+
+    final angle = -math.atan2(dy, dx) < 0
+        ? -math.atan2(dy, dx) + math.pi * 2
+        : -math.atan2(dy, dx);
+
+    xAlign = -1.4 * math.cos(angle + cardAngle);
+    yAlign = 1.2 * math.sin(angle + cardAngle);
+
+    xAngle = -math.pi / 6 * math.cos(angle + cardAngle);
+    yAngle = math.pi / 6 * math.sin(angle + cardAngle);
+    // zAngle = -math.pi / 6 * math.tan(angle + cardAngle);
+
+    // print('$xAngle, $yAngle');
+
+    curve = Curves.easeInOut;
+
+    await animationController.forward();
+  }
+
+  Future<void> updateValueForward({
     required BoxConstraints constraints,
     required double x,
     required double y,
-  }) {
+  }) async {
     final center = Offset(
       constraints.maxWidth / 2,
       constraints.maxHeight / 2,
@@ -104,13 +142,13 @@ class _CardAnimationTestPage extends State<CardAnimationTestPage>
 
     curve = Curves.easeInOut;
 
-    animationController.forward();
+    await animationController.forward();
   }
 
-  void updateValueReverse() {
+  Future<void> updateValueReverse() async {
     curve = Curves.bounceOut.flipped;
 
-    animationController.reverse();
+    await animationController.reverse();
   }
 
   @override
@@ -123,6 +161,8 @@ class _CardAnimationTestPage extends State<CardAnimationTestPage>
       body: SafeArea(
         child: LayoutBuilder(
           builder: (_, constraints) {
+            this.constraints = constraints;
+
             return GestureDetector(
               behavior: HitTestBehavior.opaque,
               onPanDown: (details) {
@@ -146,6 +186,13 @@ class _CardAnimationTestPage extends State<CardAnimationTestPage>
               },
               onPanEnd: (details) {
                 updateValueReverse();
+              },
+              onDoubleTap: () async {
+                // await updateValueReverse();
+
+                // yAngle = math.pi;
+
+                // animationController.forward();
               },
               child: Center(
                 child: Transform(
@@ -171,22 +218,25 @@ class _CardAnimationTestPage extends State<CardAnimationTestPage>
                     ),
                   child: Transform.rotate(
                     angle: cardAngle,
-                    child: Container(
-                      width: 300,
-                      height: 200,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        gradient: RadialGradient(
+                    child: ShaderMask(
+                      blendMode: BlendMode.srcATop,
+                      shaderCallback: (bounds) {
+                        return RadialGradient(
                           radius: Tween(begin: 1.5, end: 1.5)
                               .chain(CurveTween(curve: Curves.easeInOut))
                               .evaluate(animationController)
                               .toDouble(),
                           colors: [
-                            Colors.red.shade100,
-                            Colors.red.shade200,
-                            Colors.red,
-                            Colors.red.shade800,
-                            Colors.red.shade900,
+                            // Colors.red.shade100,
+                            // Colors.red.shade200,
+                            // Colors.red,
+                            // Colors.red.shade800,
+                            // Colors.red.shade900,
+                            Colors.white.withOpacity(0.4),
+                            Colors.white.withOpacity(0.3),
+                            Colors.transparent,
+                            Colors.black.withOpacity(0.1),
+                            Colors.black.withOpacity(0.2),
                           ],
                           stops: const [
                             0,
@@ -205,81 +255,55 @@ class _CardAnimationTestPage extends State<CardAnimationTestPage>
                                 .evaluate(animationController)
                                 .toDouble(),
                           ),
+                        ).createShader(bounds);
+                      },
+                      child: Container(
+                        width: 300,
+                        height: 200,
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: const [
+                            BoxShadow(
+                              offset: Offset(0, 0),
+                              color: Colors.black,
+                              blurRadius: 0.5,
+                              spreadRadius: 0.5,
+                            ),
+                          ],
                         ),
-                        boxShadow: const [
-                          BoxShadow(
-                            offset: Offset(0, 0),
-                            color: Colors.black,
-                            blurRadius: 0.5,
-                            spreadRadius: 0.5,
-                          ),
-                        ],
-                      ),
-                      child: Stack(
-                        children: [
-                          Positioned(
-                            top: 0,
-                            bottom: 0,
-                            left: 20,
-                            child: Container(
-                              alignment: Alignment.centerLeft,
+                        child: Stack(
+                          children: [
+                            Positioned(
+                              top: 0,
+                              bottom: 0,
+                              left: 20,
                               child: Container(
-                                width: 35,
-                                height: 30,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(5),
-                                  // gradient: RadialGradient(
-                                  //   radius: Tween(begin: 1.5, end: 1.5)
-                                  //       .chain(
-                                  //           CurveTween(curve: Curves.easeInOut))
-                                  //       .evaluate(animationController)
-                                  //       .toDouble(),
-                                  //   colors: [
-                                  //     Colors.yellow.shade100,
-                                  //     Colors.yellow.shade200,
-                                  //     Colors.yellow,
-                                  //     Colors.yellow.shade800,
-                                  //     Colors.yellow.shade900,
-                                  //   ],
-                                  //   stops: const [
-                                  //     0,
-                                  //     0.15,
-                                  //     0.5,
-                                  //     0.85,
-                                  //     1.0,
-                                  //   ],
-                                  //   center: Alignment(
-                                  //     Tween(
-                                  //       begin: -(35 / 2 + 95) / 300,
-                                  //       end: xAlign + -(35 / 2 + 95) / 300,
-                                  //     )
-                                  //         .chain(CurveTween(curve: curve))
-                                  //         .evaluate(animationController)
-                                  //         .toDouble(),
-                                  //     Tween(begin: 0, end: yAlign)
-                                  //         .chain(CurveTween(curve: curve))
-                                  //         .evaluate(animationController)
-                                  //         .toDouble(),
-                                  //   ),
-                                  // ),
-                                  color: Colors.yellow.shade800,
+                                alignment: Alignment.centerLeft,
+                                child: Container(
+                                  width: 35,
+                                  height: 30,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5),
+                                    color: Colors.yellow.shade800,
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                          const Positioned(
-                            bottom: 20,
-                            left: 20,
-                            child: Text(
-                              'JANG JUHWAN',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 15,
-                                color: Color.fromRGBO(0, 0, 0, 0.7),
+                            const Positioned(
+                              bottom: 20,
+                              left: 20,
+                              child: Text(
+                                'JANG JUHWAN',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                  color: Color.fromRGBO(0, 0, 0, 0.7),
+                                ),
                               ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
