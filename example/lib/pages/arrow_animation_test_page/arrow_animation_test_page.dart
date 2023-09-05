5d8b0bf3b773412162ab7f1b5782c0f246ef4f7d
orange-amuz <orange@amuz.co.kr>
2023-09-05 16:28:11 +0900

    애니메이션 컨트롤러를 하나로 변경, gitignore 수정

--- example/lib/pages/arrow_animation_test_page/arrow_animation_test_page.dart
+++ example/lib/pages/arrow_animation_test_page/arrow_animation_test_page.dart
@@ -8,19 +8,14 @@ class ArrowAnimationTestPage extends StatefulWidget {
 }
 
 class _ArrowAnimationTestPageState extends State<ArrowAnimationTestPage>
-    with TickerProviderStateMixin {
+    with SingleTickerProviderStateMixin {
   static const double iconSize = 50;
 
   static const int durationValue = 1500;
   static const animationDuration = Duration(milliseconds: durationValue);
   static const animationCurve = Curves.linear;
 
-  late final firstAnimationController = AnimationController(
-    vsync: this,
-    duration: animationDuration,
-  );
-
-  late final secondAnimationController = AnimationController(
+  late final aniamtionController = AnimationController(
     vsync: this,
     duration: animationDuration,
   );
@@ -29,22 +24,22 @@ class _ArrowAnimationTestPageState extends State<ArrowAnimationTestPage>
   void initState() {
     super.initState();
 
-    firstAnimationController.repeat();
-    secondAnimationController.value = 1.0;
+    aniamtionController.repeat();
+    // secondAnimationController.value = 1.0;
 
-    Future.delayed(Duration(milliseconds: (durationValue / 2).round())).then(
-      (value) {
-        if (mounted) {
-          secondAnimationController.repeat();
-        }
-      },
-    );
+    // Future.delayed(Duration(milliseconds: (durationValue / 2).round())).then(
+    //   (value) {
+    //     if (mounted) {
+    //       secondAnimationController.repeat();
+    //     }
+    //   },
+    // );
   }
 
   @override
   void dispose() {
-    firstAnimationController.dispose();
-    secondAnimationController.dispose();
+    aniamtionController.dispose();
+    // secondAnimationController.dispose();
 
     super.dispose();
   }
@@ -64,12 +59,12 @@ class _ArrowAnimationTestPageState extends State<ArrowAnimationTestPage>
                 return Stack(
                   alignment: Alignment.center,
                   children: [
-                    _buildAnimatedIcon(
-                      firstAnimationController,
+                    _buildFirstAnimatedIcon(
+                      aniamtionController,
                       constraints.maxWidth,
                     ),
-                    _buildAnimatedIcon(
-                      secondAnimationController,
+                    _buildSecondAnimatedIcon(
+                      aniamtionController,
                       constraints.maxWidth,
                     ),
                   ],
@@ -82,7 +77,7 @@ class _ArrowAnimationTestPageState extends State<ArrowAnimationTestPage>
     );
   }
 
-  Widget _buildAnimatedIcon(
+  Widget _buildFirstAnimatedIcon(
     AnimationController controller,
     double maxWidth,
   ) {
@@ -121,6 +116,62 @@ class _ArrowAnimationTestPageState extends State<ArrowAnimationTestPage>
               .transform(
                 controller.value * 2 - 1,
               );
+        }
+
+        return Positioned(
+          left: posValue,
+          child: Opacity(
+            opacity: opacityValue,
+            child: _buildIcon(),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildSecondAnimatedIcon(
+    AnimationController controller,
+    double maxWidth,
+  ) {
+    final posTween = Tween(
+      begin: 0.0,
+      end: maxWidth - iconSize,
+    );
+
+    final inOpacityTween = Tween(
+      begin: 0.0,
+      end: 1.0,
+    );
+
+    final outOpacityTween = Tween(
+      begin: 1.0,
+      end: 0.0,
+    );
+
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (_, child) {
+        late final double value;
+
+        if (controller.value >= 0.5) {
+          value = controller.value - 0.5;
+        } else {
+          value = controller.value + 0.5;
+        }
+
+        final posValue =
+            posTween.chain(CurveTween(curve: animationCurve)).transform(value);
+
+        late final double opacityValue;
+
+        if (value <= 0.5) {
+          opacityValue = inOpacityTween
+              .chain(CurveTween(curve: animationCurve))
+              .transform(value * 2);
+        } else {
+          opacityValue = outOpacityTween
+              .chain(CurveTween(curve: animationCurve))
+              .transform(value * 2 - 1);
         }
 
         return Positioned(
