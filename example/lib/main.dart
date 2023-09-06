cfb34b1ba5a333338a0402ff475a3b93c7927675
orange-amuz <orange@amuz.co.kr>
2023-09-06 11:58:27 +0900

    lottie 애니메이션 페이지 추가

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:example/pages/card_animation_test_page/card_animation_test_page.
 import 'package:example/pages/container_animation_test_page/container_animation_test_page.dart';
 import 'package:example/pages/dice_animation_test_page/dice_animation_test_page.dart';
 import 'package:example/pages/hover_animation_test_page/hover_animation_test_page.dart';
+import 'package:example/pages/lottie_animation_test_page/lottie_animation_test_page.dart';
 import 'package:example/pages/music_player_animation_test_page/music_player_animation_test_page.dart';
 import 'package:example/pages/nico_music_player_animation_test_page/nico_music_player_animation_test_page.dart';
 import 'package:example/pages/square_animation_test_page/square_animation_test_page.dart';
@@ -303,6 +304,19 @@ class _MyHomePageState extends State<MyHomePage>
               context,
               MaterialPageRoute(
                 builder: (_) => const ContainerAnimationTestPage(),
+              ),
+            );
+          },
+        ),
+        CupertinoButton(
+          child: const Text(
+            'Go To Lottie Animation Page',
+          ),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => const LottieAnimationTestPage(),
               ),
             );
           },
---
+++ example/lib/pages/lottie_animation_test_page/lottie_animation_test_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class LottieAnimationTestPage extends StatefulWidget {
+  const LottieAnimationTestPage({super.key});
+
+  @override
+  State<LottieAnimationTestPage> createState() =>
+      _LottieAnimationTestPageState();
+}
+
+class _LottieAnimationTestPageState extends State<LottieAnimationTestPage>
+    with SingleTickerProviderStateMixin {
+  late final controller = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 500),
+  );
+
+  @override
+  void initState() {
+    super.initState();
+
+    // WidgetsBinding.instance.addPostFrameCallback(
+    //   (timeStamp) {
+    //     controller.forward();
+    //   },
+    // );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            GestureDetector(
+              child: Lottie.network(
+                'https://raw.githubusercontent.com/juhwan976/BAEMIN/master/baemin_app/assets/MyBaemin/bronze/data.json',
+                controller: controller,
+                repeat: false,
+                onLoaded: (p0) => controller.forward(),
+              ),
+              onTap: () {
+                controller.reset();
+                controller.forward();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
